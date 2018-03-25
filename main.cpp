a6c5fb73a6486d96500e603006965767b738ac4a
Silviu Rad <silviu_rad@yahoo.com>
2018-03-25 18:20:39 +0300

    Exerctiul 3

--- main.cpp
+++ main.cpp
@@ -3,13 +3,20 @@
 using namespace std;
 
 int main(){
-   int a[20], i;
-   for (i = 0; i < 20; i++) {
-      a[i] = i + 1;
-   }
-   for (i = 0; i < 20; i++) {
-      cout << a[i] << " ";
-   }
-   cout << endl;
-   return 0;
+    int a[20], nr, i;
+    cout << "Numarul de elemente din sirul a (maximum 20) ";
+    cin >> nr;
+    for (i = 0; i < nr; i++) {
+        cout << "a[" << i << "] = ";
+        cin >> a[i];
+    }
+    for (i = 0; i < nr; i++) {
+        if(a[i] < 0)
+            a[i] = -a[i];
+    }
+    for (i = 0; i < nr; i++) {
+        cout << a[i] << ", ";
+    }
+    cout << endl;
+    return 0;
 }
